227b93af3490591a92e79930a76ac89ce60d875f
LucaBonifazio <luca.bonifazio13@gmail.com>
2023-01-09 13:25:03 +0100

    First commit

--- index.php
+++ index.php
@@ -5,3 +5,22 @@ metodi d'istanza che abbiamo visto stamattina e create un file index.php in cui:
    => all'interno della classe è definito un costruttore
    => all'interno della classe è definito almeno un metodo
 - vengono istanziati almeno due oggetti ‘Movie’ e stampati a schermo i valori delle relative proprietà -->
+<?php
+    class Movie {
+        public $title;
+        public $originalTitle;
+        public $language;
+        public $genre;
+        public $year;
+        public $rates;
+
+        function __construct($title, $language) {
+            $this->title = $title;
+            $this->language = $language;
+        }
+    }
+
+    $armageddon = new Movie('Armageddon', 'english');
+    echo $armageddon->title;
+    echo '<br>';
+    echo $armageddon->language;
