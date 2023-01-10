4cb7c37bcb7be4e12c8aead0b91391b53d2e3f5f
LucaBonifazio <luca.bonifazio13@gmail.com>
2023-01-10 09:40:15 +0100

    fix function

--- index.php
+++ index.php
@@ -1,10 +1,3 @@
-<!-- Oggi pomeriggio ripassate i primi concetti di classe e di variabili e
-metodi d'istanza che abbiamo visto stamattina e create un file index.php in cui:
-- è definita una classe ‘Movie’
-   => all'interno della classe sono dichiarate delle variabili d'istanza
-   => all'interno della classe è definito un costruttore
-   => all'interno della classe è definito almeno un metodo
-- vengono istanziati almeno due oggetti ‘Movie’ e stampati a schermo i valori delle relative proprietà -->
 <?php
     class Movie {
         public $title;
@@ -14,7 +7,7 @@ metodi d'istanza che abbiamo visto stamattina e create un file index.php in cui:
         //public $year;
         private $year;  
 
-        function __construct($title,$originalTitle, $language,$genre,$year) {
+        public function __construct($title,$originalTitle, $language,$genre,$year) {
             $this->title = $title;
             $this->originalTitle = $originalTitle;
             $this->language = $language;
@@ -59,4 +52,3 @@ metodi d'istanza che abbiamo visto stamattina e create un file index.php in cui:
     echo '<br>';
     echo $coco->getYear(2017);
     echo '<br>';
-    var_dump($coco);
